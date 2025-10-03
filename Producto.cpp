82aedae3d13b00a02707831fc3f17dddbc395a94
Daniel Fonseca <josedaniel010146@gmail.com>
2025-10-03 12:17:36 -0600

    Creacion producto.cpp

--- Producto.cpp
+++ Producto.cpp
@@ -1,2 +1,67 @@
-#ifndef PRODUCTO_H
-#define PRODUCTO_H
+#include "Producto.h"
+#include <iostream>
+#include <windows.h>
+
+// Destructor
+Producto::~Producto()
+{
+    delete siguiente;
+}
+
+int Producto::getId()
+{
+    return id;
+}
+
+std::string Producto::getNombre()
+{
+    return nombre;
+}
+
+double Producto::getPrecio()
+{
+    return precio;
+}
+
+int Producto::getCantidad()
+{
+    return cantidad;
+}
+
+Producto *Producto::getSiguiente()
+{
+    return siguiente;
+}
+
+// Setters
+void Producto::setId(int nuevoId)
+{
+    id = nuevoId;
+}
+void Producto::setNombre(std::string nuevoNombre)
+{
+    nombre = nuevoNombre;
+}
+void Producto::setPrecio(double nuevoPrecio)
+{
+    precio = nuevoPrecio;
+}
+void Producto::setCantidad(int nuevaCantidad)
+{
+    cantidad = nuevaCantidad;
+}
+void Producto::setSiguiente(Producto *nuevoSiguiente)
+{
+    siguiente = nuevoSiguiente;
+}
+
+// imprimir
+
+void Producto::imprimir()
+{
+    std::cout << "ID: " << id << std::endl;
+    std::cout << "Nombre: " << nombre << std::endl;
+    std::cout << "Precio: " << precio << std::endl;
+    std::cout << "Cantidad: " << cantidad << "\n"
+              << std::endl;
+}
